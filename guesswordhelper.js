3ca7286854318582bd888afa0accd70c07cb47cb
Jérémy Libion <10293914+MisteRibi@users.noreply.github.com>
2025-03-17 20:36:42 -0400

    added a deleteall button and a check button | words list start at three characters

--- guesswordhelper.js
+++ guesswordhelper.js
@@ -7,7 +7,7 @@ window.addEventListener("load", (event) => {
     fetch("french.txt")
     .then(response => response.text())
     .then(text => {
-        french = new Set(removeAccents(text).toLowerCase().split(/\r?\n/));
+        french = new Set(removeAccents(text).toLowerCase().split(/\r?\n/).filter(string => (string.length > 2 ? string : null)));
         //console.log('french:')
         //console.log(french);
         wordsCount();
@@ -15,7 +15,7 @@ window.addEventListener("load", (event) => {
     .catch(error => console.error(error));
 });
 
-function wordsCount() {
+function wordsCount(checking = false) {
     //console.log('length: '+french.length())
     let possible = document.getElementById("words");
     let lettersInput = document.getElementById('word');
@@ -29,25 +29,29 @@ function wordsCount() {
     possible.innerText = new Intl
         .NumberFormat(language)
         .format(words.length);
-    setState(lettersInput, letters, words.length);
+    setState(lettersInput, letters, words.length, checking);
 };
 
 function removeAccents(text) {
     return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 };
 
-function setState(lettersInput, letters, availableWords) {
+function setState(lettersInput, letters, availableWords, checking) {
     let win = document.getElementById('win');
     let lost = document.getElementById('lost');
+    let not = document.getElementById('not');
     win.classList.add('hidden');
     lost.classList.add('hidden');
+    not.classList.add('hidden');
     if (availableWords === 0) {
         lettersInput.setAttribute('aria-invalid',"true");
         lost.classList.remove('hidden');
-    } else if (availableWords === 1) {
+    } else if (availableWords === 1 || checking) {
         if (french.has(letters)) {
             lettersInput.setAttribute('aria-invalid',"false");
             win.classList.remove('hidden');
+        } else if (checking) {
+            not.classList.remove('hidden');
         };
     };
 };
@@ -60,14 +64,18 @@ function setLanguage() {
         document.title = 'Assistant du jeu "Devine le mot"';
         document.getElementById('win').innerText = "Tu as gagné!";
         document.getElementById('lost').innerText = "Tu as perdu!";
+        document.getElementById('not').innerText = "Pas un mot! Cherche encore.";
         document.getElementById('sentence').innerText = 'Nombre de mots possible: ';
         document.getElementById(key).innerText = "EN";
+        document.getElementById('check').innerText = "Vérifier";
     } else {
         document.title = 'Guess Word Game Helper';
         document.getElementById('win').innerText = "You won!";
         document.getElementById('lost').innerText = "You lost!";
+        document.getElementById('not').innerText = "Not a word! Keep looking";
         document.getElementById('sentence').innerText = 'Number of words possible: ';
-        document.getElementById(key).innerText = "FR";
+        document.getElementById(key).innerText = "EN";
+        document.getElementById('check').innerText = "Check";
     };
     document.getElementById('title').innerText = document.title;
     
--- keys.js
+++ keys.js
@@ -11,12 +11,15 @@ function createKeys() {
         board += `<button id="${letter}" onclick="sendKey('${letter}');">${letter}</button>`;
     });
     board += `<button id="delete" onclick="sendKey('delete');"></button>`;
+    board += `<button id="deleteall" onclick="sendKey('deleteall');"></button>`;
     document.getElementById('keys').innerHTML = board;
 };
 
 function sendKey(key) {
     let lettersInput = document.getElementById('word');
     if (key === 'delete') lettersInput.value = lettersInput.value.substring(0, lettersInput.value.length - 1);
+    else if (key === 'deleteall') lettersInput.value = "";
     else lettersInput.value = lettersInput.value + key;
     lettersInput.dispatchEvent(new KeyboardEvent('keyup'));
 };
+
